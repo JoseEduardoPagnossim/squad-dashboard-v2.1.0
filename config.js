ac7f6333f716d8cbadcb075781f57b8d51700452
Jose Eduardo Pagnossim <josecastillapagnossim@gmail.com>
2026-08-13 14:42:16 -0300

    Ajuste config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
   e preencha a URL + chave publishable/anon do projeto.
 */
 window.APP_CONFIG = {
-  mode: 'demo',
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  mode: 'supabase',
+  supabaseUrl: 'https://ulibturunaioulmoeykq.supabase.co',
+  supabaseAnonKey: 'sb_publishable_kOXVdDyPx6ljWYCoUR_Lbg_aqGZgvIv'
 };
